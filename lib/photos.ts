df7fcc15cd1938f660d0ac1ac11e203268cb8ed6
mmazco <maryam.maz337@gmail.com>
2025-06-23 12:32:19 -0700

    Fix Tochal Ski Resort filenames - remove brackets
    - Updated to 'Tochal Ski Resort 1' and 'Tochal Ski Resort 2'
    - Fixes broken image display for 23062025

--- lib/photos.ts
+++ lib/photos.ts
@@ -8,7 +8,7 @@ export const photoData: PhotoData = {
       details: "Canon T50, Lomography Colour Negative film, ISO 400 35mm"
     },
     {
-      filename: "/images/Tochal Ski Resort (1), Tehran, Iran Dec 2016.jpg",
+      filename: "/images/Tochal Ski Resort 1, Tehran, Iran Dec 2016 .jpg",
       title: "Tochal Ski Resort, Tehran, Iran Dec 2016",
       details: "Taken on iPhone"
     },
@@ -28,9 +28,9 @@ export const photoData: PhotoData = {
       details: "Canon T50, Lomography Colour Negative film, ISO 400 35mm"
     },
     {
-      filename: "/images/Tochal Ski Resort (2), Tehran, Iran Dec 2016.jpg",
+      filename: "/images/Tochal Ski Resort 2, Tehran, Iran Dec 2016.jpg",
       title: "Tochal Ski Resort, Tehran, Iran Dec 2016",
-      details: "Film photography"
+      details: "Taken on iPhone"
     },
     {
       filename: "/images/Antique Shop, Tehran, Iran March 2015 Canon T50, BW Lady Grey Negative film with ISO 400.jpg",
