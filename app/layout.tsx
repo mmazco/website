590fb0e433158d0df829b13913fde2939d50af2f
mmazco <maryam.maz337@gmail.com>
2026-02-02 14:38:12 -0800

    Organize favicon assets into dedicated folder
    - Move all favicon files to assets/favicons/
    - Update layout.tsx favicon references
    - Update site.webmanifest icon paths
    - Complete assets organization structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
-        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/assets/favicons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/assets/favicons/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/assets/favicons/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/assets/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/assets/favicons/site.webmanifest" />
       </head>
       <body>
         <PHProvider>
